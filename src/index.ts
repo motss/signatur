e2593d4aee301dddac2cca866116539b3bd7521b
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-03-14 22:48:29 +0800

    :construction: WIP - working on the implementation of signSync and unsignSync

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,110 @@
 // @ts-check
 
-export async function greeting(
-  name: string = 'World'
+export declare interface SignaturOptionsError {
+  error: {
+    type: string;
+    message: string;
+  };
+}
+export declare interface SignaturOptions {
+  secret: string;
+  separator?: string;
+  error?: SignaturOptionsError;
+}
+
+/** Import project dependencies */
+import crypto from 'crypto';
+
+export function signSync<T>(
+  rawData: T,
+  options: SignaturOptions = {} as SignaturOptions
 ) {
-  try {
-    return await greetingSync(name);
-  } catch (e) {
-    throw e;
+  const {
+    secret,
+    separator = '.',
+  } = options;
+
+  if (typeof secret !== 'string' || !secret.length) {
+    throw new TypeError('Param secret is not a string');
   }
+
+  if (rawData == null) {
+    throw new TypeError('Param rawData is undefined');
+  }
+
+  const stringifiedData = typeof rawData !== 'string'
+    ? JSON.stringify(rawData)
+    : rawData;
+
+  const hashed = Buffer
+    .from(stringifiedData, 'utf8')
+    .toString('base64')
+    .replace(/\+/gi, '-')
+    .replace(/\//gi, '_')
+    .replace(/=/gi, '');
+  const enc = crypto
+    .createHmac('sha256', secret)
+    .update(stringifiedData)
+    .digest('base64')
+    .replace(/\+/gi, '-')
+    .replace(/\//gi, '_')
+    .replace(/=/gi, '');
+
+  return `${hashed}${separator}${enc}`;
 }
 
-export function greetingSync(
-  name: string = 'World'
+export function unsignSync<T>(
+  signature: string,
+  options: SignaturOptions = {} as SignaturOptions
 ) {
-  return `Hello, ${name}!`;
-}
+  const {
+    secret,
+    separator = '.',
+    error,
+  } = options;
 
-export default greeting;
+  if (typeof secret !== 'string' || !secret.length) {
+    throw new TypeError('Param secret is not a string');
+  }
+
+  if (typeof signature !== 'string' || !signature.length) {
+    throw new TypeError('Param signature is not a string');
+  }
+
+  const [hash, enc] = signature.split(separator, 2);
+  const decoded = Buffer.from(
+    (hash + '==='.slice((hash.length + 3) % 4))
+      .replace(/\-/gi, '+')
+      .replace(/_/gi, '/'),
+    'base64'
+  )
+    .toString('utf8');
+  const signedDecoded = crypto
+    .createHmac('sha256', secret)
+    .update(decoded)
+    .digest('base64')
+    .replace(/\+/gi, '-')
+    .replace(/\//gi, '_')
+    .replace(/=/gi, '');
+
+  if (enc !== signedDecoded) {
+    throw error == null
+      ? {
+        error: {
+          type: 'invalid-signature',
+          message: 'Signature not match',
+        },
+      }
+      : error as any;
+  }
+
+  const parsed = (() => {
+    try {
+      return JSON.parse(decoded) as T;
+    } catch (e) {
+      return decoded as string;
+    }
+  })();
+
+  return parsed;
+}
